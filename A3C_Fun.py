5dc52ffd39748f31ee2a088efeae566bcc9e4bd1
DeailLee <dleodlf1004@naver.com>
2019-04-16 11:22:53 +0900

    2019_04_16 오전 save_ 1) 여러개 메모리 자동으로 만들기 2) 네트워크 공유성 검증을 위해서 구축 중

---
+++ A3C_Fun.py
@@ -0,0 +1,62 @@
+import multiprocessing
+import time
+import CNS_Send_UDP
+
+
+class function1(multiprocessing.Process):
+    ## 단순한 값만 읽어 오는 예제
+    def __init__(self, mem):
+        multiprocessing.Process.__init__(self)
+        self.mem = mem[0]   # main mem connection
+
+    def run(self):
+        while True:
+            # print(self, self.mem['QPROREL'])
+            time.sleep(1)
+
+
+class function2(multiprocessing.Process):
+    ## 제어 신호 보내는 예제
+    def __init__(self, mem):
+        multiprocessing.Process.__init__(self)
+        self.mem = mem
+        self.mem2 = mem[2]
+
+        # UDP send part
+        self.UDP_sock = CNS_Send_UDP.CNS_Send_Signal('192.168.0.55', 7001)
+
+    def run(self):
+        while True:
+            # print(self, self.mem[1]['Test'], '->', 1, self.mem2)
+            self.mem[1]['Test'] = 1
+            self.mem[2].append(1)
+            self.UDP_sock._send_control_signal(['KSWO33', 'KSWO32'], [1, 0])
+            time.sleep(1)
+
+
+class function3(multiprocessing.Process):
+    def __init__(self, mem):
+        multiprocessing.Process.__init__(self)
+        self.mem = mem
+        self.mem2 = mem[2]
+
+    def run(self):
+        while True:
+            # print(self, self.mem[1]['Test'], '->', 2, self.mem2)
+            self.mem[1]['Test'] = 2
+            self.mem[2].append(2)
+            time.sleep(3)
+
+#========================================================================
+
+
+class t_function1(multiprocessing.Process):
+    def __init__(self, mem):
+        multiprocessing.Process.__init__(self)
+        self.mem = mem[0] # main mem connection
+
+    def run(self):
+        para = ['KMSISO']
+        while True:
+            print(self, self.mem['KCNTOMS']['V'])
+            time.sleep(1)
--- A3C_Main.py
+++ A3C_Main.py
@@ -1,40 +1,33 @@
+import A3C_Fun as A3
 from db import db_make
 from multiprocessing import Manager
 from CNS_UDP import *
-from CNS_Fun import *
-from CNS_GFun import *
 from CNS_CFun import *
-from copy import deepcopy
+
 
 class body:
     def __init__(self):
         #==== Initial part for testing===========================================================#
-        self.a3c_test_mode = True
-        self.shut_up = [True for _ in range(0, 3)]
+        # self.a3c_mode : a3c모드의 여부와 에이전트의 갯수를 조정하는 곳이다.
+        self.a3c_mode = {'mode': True, 'Nub_agent': 2, 'Range': range(0, 2)}
+        self.shut_up = [True for _ in self.a3c_mode['Range']]
         #========================================================================================#
-        self.shared_mem = [generate_mem().make_mem_structure() for _ in range(0, 3)]
+        self.shared_mem = [generate_mem().make_mem_structure() for _ in self.a3c_mode['Range']]
         #========================================================================================#
-        self.UDP_net = [UDPSocket(self.shared_mem[_], IP='', Port=7001+_, shut_up=self.shut_up[_]) for _ in range(0, 3)]
+        self.UDP_net = [UDPSocket(self.shared_mem[_], IP='', Port=7001+_,
+                                  shut_up=self.shut_up[_]) for _ in self.a3c_mode['Range']]
 
-        if self.a3c_test_mode:
-            self.process_list = [
-                clean_mem(self.shared_mem[0], shut_up=self.shut_up[0]),
-                clean_mem(self.shared_mem[1], shut_up=self.shut_up[1]),
-                function1(self.shared_mem[0]),
-                function1(self.shared_mem[1]),
-                # function2(self.shared_mem[1]),
-                # function3(self.shared_mem),
-                # gfunction(self.shared_mem),
-                # gfunction2(self.shared_mem),
-            ]
+        if self.a3c_mode['mode']:
+            clean_mem_list = [clean_mem(self.shared_mem[_], shut_up=self.shut_up[_]) for _ in self.a3c_mode['Range']]
+            a3c_function_list = [A3.function1(self.shared_mem[_]) for _ in self.a3c_mode['Range']]
+            self.process_list = clean_mem_list + a3c_function_list
         else:
             self.process_list = [
                 clean_mem(self.shared_mem, shut_up=self.shut_up),
-
             ]
 
     def start(self):
-        print('A3C test mode : {}'.format(self.a3c_test_mode))
+        print('A3C test mode : {}'.format(self.a3c_mode['mode']))
         job_list = []
         for __ in self.UDP_net:
             __.start()
@@ -45,6 +38,7 @@ class body:
             job_list.append(__)
         for job in job_list:
             job.join()
+
 
 class generate_mem:
     def make_test_mem(self):
@@ -118,8 +112,62 @@ class generate_mem:
             for __ in memory_list:
                 print('{}번째 리스트|{}'.format(i, __))
                 i += 1
-        print('done')
+        print('Mem_List 생성 완료')
         return memory_list
+# ====================================================================================================================#
+import tensorflow as tf
+from keras import backend as K
+from keras.layers import Dense, Input, Conv1D, MaxPooling1D, LSTM, Flatten
+from keras.models import Model
+from keras.optimizers import Adam, RMSprop
+from keras import backend as K
+
+
+class A3C_main_network:
+    def __init__(self):
+        pass
+
+    def build_network_model(self, net_type='DNN', in_pa=1, ou_pa=1, time_leg=1):
+        # 네트워크 모델 - 의존성 없음
+        if True:
+            if net_type == 'DNN':
+                state = Input(batch_shape=(None, in_pa))
+                shared = Dense(32, input_dim=in_pa, activation='relu', kernel_initializer='glorot_uniform')(state)
+                # shared = Dense(48, activation='relu', kernel_initializer='glorot_uniform')(shared)
+
+            elif net_type == 'CNN' or net_type == 'LSTM' or net_type == 'CLSTM':
+                state = Input(batch_shape=(None, time_leg, in_pa))
+                if net_type == 'CNN':
+                    shared = Conv1D(filters=10, kernel_size=3, strides=1, padding='same')(state)
+                    shared = MaxPooling1D(pool_size=2)(shared)
+                    shared = Flatten()(shared)
+
+                elif net_type == 'LSTM':
+                    shared = LSTM(16, activation='relu')(state)
+
+                elif net_type == 'CLSTM':
+                    shared = Conv1D(filters=10, kernel_size=3, strides=1, padding='same')(state)
+                    shared = MaxPooling1D(pool_size=2)(shared)
+                    shared = LSTM(8)(shared)
+
+            # ----------------------------------------------------------------------------------------------------
+            # Common output network
+            actor_hidden = Dense(8, activation='relu', kernel_initializer='glorot_uniform')(shared)
+            action_prob = Dense(ou_pa, activation='softmax', kernel_initializer='glorot_uniform')(actor_hidden)
+
+            value_hidden = Dense(4, activation='relu', kernel_initializer='he_uniform')(shared)
+            state_value = Dense(1, activation='linear', kernel_initializer='he_uniform')(value_hidden)
+
+            actor, critic = Model(inputs=state, outputs=action_prob), Model(inputs=state, outputs=state_value)
+            print('Make {} Network'.format(net_type))
+
+            actor._make_predict_function()
+            critic._make_predict_function()
+
+        # actor.summary(print_fn=logging.info)
+        # critic.summary(print_fn=logging.info)
+
+        return actor, critic
 
 
 if __name__ == '__main__':
