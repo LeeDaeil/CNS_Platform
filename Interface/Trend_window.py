8ea8980e3461c5992be3dbc55fc3d924891e1e5c
DeailLee <dleodlf1004@naver.com>
2019-05-15 15:45:33 +0900

    2019_05_14 Test

--- Interface/Trend_window.py
+++ Interface/Trend_window.py
@@ -11,13 +11,25 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
-        Dialog.resize(497, 471)
+        Dialog.resize(497, 623)
         self.Test_label = QtWidgets.QLabel(Dialog)
         self.Test_label.setGeometry(QtCore.QRect(10, 10, 56, 12))
         self.Test_label.setObjectName("Test_label")
         self.listWidget = QtWidgets.QListWidget(Dialog)
-        self.listWidget.setGeometry(QtCore.QRect(10, 30, 256, 192))
+        self.listWidget.setGeometry(QtCore.QRect(10, 30, 256, 31))
         self.listWidget.setObjectName("listWidget")
+        self.scrollArea = QtWidgets.QScrollArea(Dialog)
+        self.scrollArea.setGeometry(QtCore.QRect(10, 70, 471, 531))
+        self.scrollArea.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOn)
+        self.scrollArea.setWidgetResizable(True)
+        self.scrollArea.setObjectName("scrollArea")
+        self.scrollAreaWidgetContents = QtWidgets.QWidget()
+        self.scrollAreaWidgetContents.setGeometry(QtCore.QRect(0, 0, 452, 529))
+        self.scrollAreaWidgetContents.setObjectName("scrollAreaWidgetContents")
+        self.pushButton = QtWidgets.QPushButton(self.scrollAreaWidgetContents)
+        self.pushButton.setGeometry(QtCore.QRect(10, 20, 75, 641))
+        self.pushButton.setObjectName("pushButton")
+        self.scrollArea.setWidget(self.scrollAreaWidgetContents)
 
         self.retranslateUi(Dialog)
         QtCore.QMetaObject.connectSlotsByName(Dialog)
@@ -26,4 +38,5 @@ class Ui_Dialog(object):
         _translate = QtCore.QCoreApplication.translate
         Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
         self.Test_label.setText(_translate("Dialog", "Value"))
+        self.pushButton.setText(_translate("Dialog", "PushButton"))
 
